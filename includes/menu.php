561f0d3a93633856dc1965f9b4325ec65778eb16
ferko <irkm813@gmail.com>
2024-11-02 13:35:56 +0100

    működnek az almenük

--- includes/menu.php
+++ includes/menu.php
@@ -9,15 +9,49 @@
 	<!-- Nav -->
 	<nav id="nav">
 		<ul>
-			<?php if (!empty($menuItems)): ?>
-				<?php foreach ($menuItems as $menuItem): ?>
-					<li><a href="<?php echo htmlspecialchars($menuItem['content']); ?>">
-						<?php echo htmlspecialchars($menuItem['menu_name']); ?>
-					</a></li>
-				<?php endforeach; ?>
-			<?php else: ?>
-				<li>Nincs elérhető menüelem</li>
-			<?php endif; ?>
+			
+		<?php
+			$lastmenuwasmain = false;
+			$isFirstElement = true;
+
+			foreach ($menuItems as $menuItem) {
+				$menuName = htmlspecialchars($menuItem['menu_name']);
+				$content = htmlspecialchars($menuItem['content']);
+
+				if ($menuName[0] != '/') {
+					// Bezárja a lista tag-et, ha az előző menü nem almenü volt
+					if ($lastmenuwasmain) {
+						echo "</li>";
+					}elseif ($isFirstElement == false){
+						echo "</ul>";
+					}
+					
+					// Egy fő menüelemet renderel
+					echo "<li><a href=\"$content\">$menuName</a>";
+					$lastmenuwasmain = true;
+				} else {
+					// This is a submenu item
+					if ($lastmenuwasmain) {
+						echo "<ul>"; // Nyit egy új listát a fő listaelemen belül
+						$lastmenuwasmain = false;
+					}
+
+					// Rendereli az almenüt
+					$submenuName = substr($menuName, 1);
+					echo "<li class=\"submenu\"><a href=\"$content\">$submenuName</a></li>";
+				}
+
+				$isFirstElement = false;
+			}
+
+			// Ha van nem bezárt tag a loop után, akkor azokat lezárja
+			if ($lastmenuwasmain) {
+				echo "</li>";
+			} else {
+				echo "</ul></li>";
+			}
+			?>
+
 		</ul>
 	</nav>
 
